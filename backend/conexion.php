638e3a21ed295fa7d726a15ea3bb1d551bd48c3f
Ricardo <rikisalas0@gmail.com>
2025-08-06 19:53:26 -0500

    actualizaciondeBD

--- backend/conexion.php
+++ backend/conexion.php
@@ -1,4 +1,5 @@
 <?php
+/*
     $servidor = "localhost";
     $usuario = "root";
     $clave = "";
@@ -8,5 +9,22 @@
     mysqli_select_db($conexion, $bd) or die ('No encontró la base de datos  ');
     mysqli_set_charset($conexion,"utf8");
     //echo "se conecto"
+ */
+
+    <?php
+/*
+Código anterior comentado...
+*/
+
+$servidor = "sql112.infinityfree.com";
+$usuario = "if0_39626355";
+$clave = "TU_PASSWORD_DEL_PANEL";
+$bd = "if0_39626355_bookstore";
+
+$conexion = mysqli_connect($servidor, $usuario, $clave, $bd)
+    or die ('No encontró el servidor o la base de datos');
+
+mysqli_set_charset($conexion, "utf8");
+?>
 
     ?>
